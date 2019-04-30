70edbf282aec7e06407a7a9562a37e49d3d85564
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-29 20:40:54 -0700

    learning about if statements with time and prompts

---
+++ js/conditions.js
@@ -0,0 +1,45 @@
+// if condition
+// var hour = 1;
+var hour = prompt('What hour is it now?');
+// if (hour > 18) {
+//     console.log('class has started');
+// }
+// if (hour < 21) {
+//     // continue class
+//     console.log('continue class');
+// }
+if (hour > 18 && hour < 21) {
+    console.log('class hours');
+} else if (hour > 0 && hour < 7) {
+    console.log('sleep time');
+} else {
+    console.log('non-class hours');
+}
+
+
+var minute = prompt('How many minutes past the hour is it now?')
+// switch condition
+switch (minute) {
+    case 00:
+        console.log('hour has just begun');
+        break;
+    case 30:
+        console.log('it is halfway through the current hour');
+        break;
+    default:
+        console.log('waiting for next hour');
+        break;
+}
+
+var color = 'red';
+switch (color) {
+    case 'blue':
+        console.log('blue');
+        break;
+    case 'yellow':
+        console.log('yellow');
+        break;
+    default:
+        console.log('color not found');
+        break;
+}
