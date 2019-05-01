d233e549b6bb5328a1f27076848c8f965e92c83b
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-04-30 19:33:27 -0700

    created operator.js

---
+++ js/operators.js
@@ -0,0 +1,13 @@
+// && and
+// || or
+// ! not
+
+// 2 variables x & y that store numbers & assign some values
+// check if x is greater than 18 & y is greater than 21
+// then display "Horray!"
+// else if x is greater than 14 & less than 18
+    // then check if y is greater than 18 & less than 21, display "There is time!"
+    // else display "You're late!"
+
+var x = 24;
+var y = 48;
