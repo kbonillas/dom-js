af03138e18b8d225ef6db6cdbab5e9031a0f1214
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-05-02 20:58:54 -0700

    assignment part 2

---
+++ js/review.js
@@ -0,0 +1,41 @@
+var user = {}; // empty object
+var name = prompt('Enter name: ');
+console.log({name}); // curly brackets around the variable will console what the property is as well as its value
+console.log(typeof(name));
+console.log(!name); // makes it the opposite, checking for false
+// console.log(!!name); // makes it the opposite opposite, checking for true
+// console.log(isNaN(name));
+
+// if (name === '' || name === ' ' || parseInt(name)) {
+//     alert('Enter Valid Name');
+// } else {
+//     user.name = name;
+// }
+
+excludedName = ['poopface', 'idiot'];
+var isExcluded = excludedName.find(function (excluded) {
+    return excluded === name;
+});
+
+
+if (name && isNaN(name)) {
+    user.name = name;
+    var hp = prompt('Enter your hp: ');
+    console.log(!!hp);
+    console.log(typeof hp);
+    console.log(parseInt(hp));
+    console.log(isNaN(parseInt(hp)));
+
+    if (hp && !isNaN(parseInt(hp))) {
+        user.power = parseInt(hp);
+    } else {
+        alert('Enter Valid hp');
+    }
+} else {
+    alert('Enter Valid Name');
+}
+
+// ternary
+// (name === '' || name === ' ' || parseInt(name)) ?
+//     alert('Enter Valid Name') :
+//     user.name = name;
