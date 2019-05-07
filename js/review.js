5797ac8696f2151b8f85a6bc69a8b9fa3edba2df
Kristin Bonillas <kristinbonillas@Kristins-MacBook-Pro.local>
2019-05-06 17:24:34 -0700

    confirming if user is correct or not

--- js/review.js
+++ js/review.js
@@ -1,8 +1,8 @@
 var user = {}; // empty object
 var name = prompt('Enter name: ');
-console.log({name}); // curly brackets around the variable will console what the property is as well as its value
-console.log(typeof(name));
-console.log(!name); // makes it the opposite, checking for false
+// console.log({name}); // curly brackets around the variable will console what the property is as well as its value
+// console.log(typeof(name));
+// console.log(!name); // makes it the opposite, checking for false
 // console.log(!!name); // makes it the opposite opposite, checking for true
 // console.log(isNaN(name));
 
@@ -21,20 +21,28 @@ var isExcluded = excludedName.find(function (excluded) {
 if (name && isNaN(name)) {
     user.name = name;
     var hp = prompt('Enter your hp: ');
-    console.log(!!hp);
-    console.log(typeof hp);
-    console.log(parseInt(hp));
-    console.log(isNaN(parseInt(hp)));
+    // console.log(!!hp);
+    // console.log(typeof hp);
+    // console.log(parseInt(hp));
+    // console.log(isNaN(parseInt(hp)));
 
     if (hp && !isNaN(parseInt(hp))) {
         user.power = parseInt(hp);
     } else {
         alert('Enter Valid hp');
     }
+    console.log(user);
+    console.log(hp);
 } else {
     alert('Enter Valid Name');
 }
 
+var userConfirm = prompt('Is ' + name + ' and HP point level of ' + hp + ' correct?');
+if (userConfirm) {
+    console.log('User Confirmed');
+} else {
+    console.log('User Denied');
+}
 // ternary
 // (name === '' || name === ' ' || parseInt(name)) ?
 //     alert('Enter Valid Name') :
